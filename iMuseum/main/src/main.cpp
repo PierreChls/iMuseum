5149681589dff3109812b8fda23c481c5a47648b
Pierre Charles <pierre.chls@gmail.com>
2015-12-01 12:13:35 +0100

    Add assimp

--- iMuseum/main/src/main.cpp
+++ iMuseum/main/src/main.cpp
@@ -14,9 +14,35 @@
 
 #include <assimp/Importer.hpp>      // C++ importer interface
 #include <assimp/scene.h>           // Output data structure
-#include <assimp/postprocess.h>     // Post processing fla
+#include <assimp/postprocess.h>     // Post processing flags
 
 using namespace glimac;
+
+bool DoTheImportThing( const std::string& pFile)
+{
+  // Create an instance of the Importer class
+  Assimp::Importer importer;
+  // And have it read the given file with some example postprocessing
+  // Usually - if speed is not the most important aspect for you - you'll 
+  // propably to request more postprocessing than we do in this example.
+  const aiScene* scene = importer.ReadFile( pFile, 
+        aiProcess_CalcTangentSpace       |
+        aiProcess_Triangulate            |
+        aiProcess_JoinIdenticalVertices  |
+        aiProcess_SortByPType);
+  
+  // If the import failed, report it
+  if( !scene)
+  {
+    std::cout << "Erreur lors du chargement de l'objet" << std::endl;
+    return false;
+  }
+  // Now we can access the file's contents. 
+  //DoTheSceneProcessing( scene);
+  // We're done. Everything will be cleaned up by the importer destructor
+  return true;
+}
+
 
 int main(int argc, char** argv)
 {
