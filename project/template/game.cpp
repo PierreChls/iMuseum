0993b243e3654a4409e76dd2c1cc49b0aa474763
DARRAUD CHARLIE <cdarraud@localhost.localdomain>
2015-12-14 16:51:01 +0100

    Validateur : DARRAUD CHARLIE <cdarraud@localhost.localdomain> 	modifié :         template/game.cpp

--- project/template/game.cpp
+++ project/template/game.cpp
@@ -36,10 +36,10 @@ int main(int argc, char** argv) {
 
 
     // Setup and compile our shaders
-    Shader MyShader("shaders/model_loading.vs.glsl", "shaders/model_loading.fs.glsl");
+    Shader MyShader("template/shaders/model_loading.vs", "template/shaders/model_loading.fs");
     
     // Load models
-    //Model model("../assets/models/nanosuit.obj");
+    Model model("assets/models/nanosuit.obj");
 
     /*********************************
      * HERE SHOULD COME THE INITIALIZATION CODE
@@ -63,23 +63,23 @@ int main(int argc, char** argv) {
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //shader.Use();
+        MyShader.Use();
 
         // Transformation matrices
-        /*glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
         
         glm::mat4 view = glm::mat4(1.0);
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(MyShader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(glGetUniformLocation(MyShader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
         // Draw the loaded model
         glm::mat4 matModel;
         // Translate model to the center of the scene
         matModel = glm::translate(matModel, glm::vec3(0.0f, -1.75f, -5.0f));
         matModel = glm::scale(matModel, glm::vec3(0.2f, 0.2f, 0.2f));
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(matModel));
+        glUniformMatrix4fv(glGetUniformLocation(MyShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(matModel));
 
-        model.Draw(shader);*/
+        model.Draw(MyShader);
 
         // Update the display
         windowManager.swapBuffers();
