b09edb624ca10ff3d9a88beb5c8aa7cf4a4d046d
Romain <belum76@gmail.com>
2015-12-17 14:13:21 +0100

    little fix

---
+++ project/template/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/game.dir/src/Light.cpp.o"
+  "CMakeFiles/game.dir/src/Shader.cpp.o"
+  "CMakeFiles/game.dir/src/Mesh.cpp.o"
+  "CMakeFiles/game.dir/src/Camera.cpp.o"
+  "CMakeFiles/game.dir/src/Model.cpp.o"
+  "CMakeFiles/game.dir/src/Scene.cpp.o"
+  "CMakeFiles/game.dir/game.cpp.o"
+  "game.pdb"
+  "game"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
