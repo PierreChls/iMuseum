36e89c89fcb497faa45fae7c764d32e67d93ba9b
Romain Francois <belum76@gmail.com>
2015-12-26 23:44:11 +0100

    remove useless shaders

--- project/template/shaders/light.fs.glsl
+++ project/template/shaders/light.fs.glsl
@@ -95,7 +95,7 @@ vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
     // Combine results
     vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, TexCoords));
-    vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords));
+    vec3 specular= light.specular * spec * vec3(texture(material.texture_specular1, TexCoords));
     return (ambient + diffuse + specular);
 }
 
