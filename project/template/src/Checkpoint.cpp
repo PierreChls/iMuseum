bed11aa9936b83cf2ee977ea684a3e9da1eceb16
Pierre Charles <pierre.chls@gmail.com>
2016-01-05 17:01:57 +0100

    Move checkpoint

--- project/template/src/Checkpoint.cpp
+++ project/template/src/Checkpoint.cpp
@@ -7,5 +7,6 @@ Checkpoint::Checkpoint(){}
 
 Checkpoint::Checkpoint(string shader_name, float rotate_angle, float rotate_x, float rotate_y, float rotate_z, float translate_x, float translate_y, float translate_z, float scale)
 {   
-    this->model = Model("assets/models/barrel/Barrell_oak_obj.obj", shader_name, rotate_angle, rotate_x, rotate_y, rotate_z, translate_x, translate_y, translate_z, scale );
+    this->model = Model("assets/models/checkpoint/Arrow.fbx", shader_name, rotate_angle, rotate_x, rotate_y, rotate_z, translate_x, translate_y, translate_z, scale );
+    //this->model = Model("assets/models/barrel/Barrell_oak_obj.obj", shader_name, rotate_angle, rotate_x, rotate_y, rotate_z, translate_x, translate_y, translate_z, scale );
 }
