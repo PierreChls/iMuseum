2dc283bdc5dcef417bc69fd3b51902562449acdb
Pierre Charles <pierre.chls@gmail.com>
2016-01-07 18:39:47 +0100

    New load config file

--- project/template/src/Scene.cpp
+++ project/template/src/Scene.cpp
@@ -13,222 +13,242 @@ Scene::Scene(string path_season)
 
 void Scene::loadScene(string path_season)
 {
-  int nbModel, nbShader, nbPointLights, nbDirLights, nbSpotLights, nbCheckpoint, i;
+  
+  //Booléen verifiant l'avancée de l'initialisation
+  bool  firstline = true,
+        shaders_initialization = true,
+        models_initialization = true,
+        pointlights_initialization = true,
+        dirlights_initialization = true,
+        spotlights_initialization = true,
+        checkpoints_initialization = true;
 
-  string word, line;
+  //Nombre des différents éléments de la scène
+  int nbShader,
+      nbModel,
+      nbPointLights, nbDirLights, nbSpotLights,
+      nbCheckpoint;
+
+
+  string line, firstword, word;
 
   //Valeurs du txt pour les shaders
   string name_shader, pathShader_vs, pathShader_fs;
 
   //Valeurs du txt pour les models
   string name_model, path_model, model_shader_name;
-  float rotate_angle, rotate_x, rotate_y, rotate_z;
-  float translate_x, translate_y, translate_z;
-  float scale;
+  float rotate_angle, rotate_x, rotate_y, rotate_z, 
+        translate_x, translate_y, translate_z,
+        scale;
 
   //Valeurs du txt pour les lights
   string name_light, light_shader_name;
   float position_x,  position_y, position_z,
-  ambient_1,   ambient_2,  ambient_3, 
-  diffuse_1,   diffuse_2,  diffuse_3,
-  specular_1,  specular_2, specular_3, 
-  constant,
-  linear,
-  quadratic,
-  direction_x, direction_y, direction_z,
-  cutOff, outerCutOff;
+        ambient_1,   ambient_2,  ambient_3, 
+        diffuse_1,   diffuse_2,  diffuse_3,
+        specular_1,  specular_2, specular_3, 
+        constant,
+        linear,
+        quadratic,
+        direction_x, direction_y, direction_z,
+        cutOff, outerCutOff;
 
   //Valeurs du txt pour les checkpoints
   string name_checkpoint, checkpoint_name_shader;
 
   ifstream file(path_season);
 
-  if(file){
+  if(file)
+  {
 
-    //INIT : First line
-    getline(file, line);
-    stringstream iss(line);
+    cout << "                           " << endl
+         << "Config file informations : " << endl
+         << "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ " << endl;
 
-    //Detection of comments in config file
-    while (iss.str().find("#") != string::npos){
-      getline(file,line);
-      iss.str(line);
-    }
-
-    while(iss >> word >> nbShader >> word >> nbModel >> word >> nbPointLights >> word >> nbDirLights >> word >> nbSpotLights >> word >> nbCheckpoint)
+    //Parcours de toutes les lignes du fichier
+    while (getline( file, line ))
     {
-      cout << " Nb Shaders      : "     << nbShader      << endl
-      << " Nb Models       : "     << nbModel       << endl
-      << " LIGHTS : "              << endl
-      << "   nbPointLights : "     << nbPointLights << endl
-      << "   nbDirLights   : "     << nbDirLights   << endl
-      << "   nbSpotLights  : "     << nbSpotLights  << endl
-      << " Nb Checkpoints  : "     << nbCheckpoint  << endl;
-    }
+      
+      istringstream iss(line);
+      iss >> firstword;
 
-    //SHADERS
-    //Detection of comments in config file
-    getline(file,line);
-    iss.str(line);
+      //Detection of comments in config file
+      if(firstword.find("#") == string::npos){
 
-    while (iss.str().find("#") != string::npos){
-      getline(file,line);
-      iss.str(line);
-    }
+        //INIT : First line
+        if(firstline)
+        {
+          istringstream iss(line); 
+          while(iss >> word >> nbShader >> word >> nbModel >> word >> nbPointLights >> word >> nbDirLights >> word >> nbSpotLights >> word >> nbCheckpoint)
+          {
+            cout << " Nb Shaders       : "     << nbShader      << endl
+                 << " Nb Models        : "     << nbModel       << endl
+                 << " Nb Lights        : "     << nbPointLights + nbDirLights + nbSpotLights << endl
+                 << "  - nbPointLights : "     << nbPointLights << endl
+                 << "  - nbDirLights   : "     << nbDirLights   << endl
+                 << "  - nbSpotLights  : "     << nbSpotLights  << endl
+                 << " Nb Checkpoints   : "     << nbCheckpoint  << endl
+            << endl;
+          }
+          firstline = false;
+          continue;
+        }
 
-    for(i = 0; i < nbShader; i++)
-    {
-      cout << "IN SHADER : " << iss.str() << endl;
-      while(iss >> name_shader >> pathShader_vs >> pathShader_fs)
-      {
+        //SHADERS
+        if(shaders_initialization)
+        {
+          for(int i = 0; i < nbShader; i++)
+          {
+            istringstream iss(line);
+            while(iss >> name_shader >> pathShader_vs >> pathShader_fs)
+            {
 
-        cout << name_shader << " " << pathShader_vs << " " << pathShader_fs << endl ;
-        this->shaders[name_shader] = Shader( (char*)pathShader_vs.c_str() , (char*)pathShader_fs.c_str() );
-      }
-      getline(file, line);
-      iss.str(line);
-    }
-    cout << "" << endl;
+              cout << name_shader << " " << pathShader_vs << " " << pathShader_fs << endl ;
+              this->shaders[name_shader] = Shader( (char*)pathShader_vs.c_str() , (char*)pathShader_fs.c_str() );
+            }
+            getline(file, line);
+          }
+          cout << "" << endl;
+          shaders_initialization = false;
+          continue;
+        }
 
-    //MODELS
-    //Detection of comments in config file
-    
-    while (iss.str().find("#") != string::npos){
-      getline(file,line);
-      iss.str(line);
-    }
-    for(i = 0; i < nbModel; i++)
-    {
-      cout << "IN MODELS :" << iss.str() << endl;
-      while(iss >> name_model >> path_model >> model_shader_name >> rotate_angle >> rotate_x >> rotate_y >> rotate_z >> translate_x >> translate_y >> translate_z >> scale)
-      {
-        cout << name_model << " " << path_model << " " << model_shader_name << endl; 
-        this->models[name_model] = Model( (char*)path_model.c_str(), (char*)model_shader_name.c_str(), rotate_angle, rotate_x, rotate_y, rotate_z, translate_x, translate_y, translate_z, scale);
-      }
+        //MODELS
+        if(models_initialization)
+        {
+          for(int i = 0; i < nbModel; i++)
+          {
+            istringstream iss(line);
+            while(iss >> name_model >> path_model >> model_shader_name >> rotate_angle >> rotate_x >> rotate_y >> rotate_z >> translate_x >> translate_y >> translate_z >> scale)
+            {
+              cout << name_model << " " << path_model << " " << model_shader_name << endl; 
+              this->models[name_model] = Model( (char*)path_model.c_str(), (char*)model_shader_name.c_str(), rotate_angle, rotate_x, rotate_y, rotate_z, translate_x, translate_y, translate_z, scale);
+            }
 
-      getline(file, line);
-      iss.str(line);
-    }
-    cout << "" << endl;
+            getline(file, line);
+          }
+          cout << "" << endl;
+          models_initialization = false;
+          continue;
+        }
 
-    /***** LIGHT *****/
+        //POINTLIGHTS
+        if(pointlights_initialization)
+        {
+          for(int i = 0; i < nbPointLights; i++)
+          {
+            istringstream iss(line);
+            while(iss >> name_light >> position_x >> position_y >> position_z >> ambient_1 >> ambient_2 >> ambient_3 >> diffuse_1 >> diffuse_2 >> diffuse_3 >> specular_1 >> specular_2 >> specular_3 >> constant >> linear >> quadratic >> light_shader_name )
+            {
+              PointLight* pointLight = new PointLight(  
+                glm::vec3( (float)position_x , (float)position_y, (float)position_z),             
+                glm::vec3( (float)ambient_1, (float)ambient_2, (float)ambient_3),                 
+                glm::vec3( (float)diffuse_1 , (float)diffuse_2 , (float)diffuse_3),               
+                glm::vec3( (float)specular_1, (float)specular_2, (float)specular_3),              
+                (float)constant,
+                (float)linear,
+                (float)quadratic,
+                (string)light_shader_name
+                );
+              this->lights[name_light] = pointLight;
+              cout << name_light << endl;
+            }
 
-    //Detection of comments in config file
-    while (iss.str().find("#") != string::npos){
-      getline(file,line);
-      iss.str(line);
-    }
+            getline(file, line);
+          }
+          cout << "" << endl;
+          pointlights_initialization = false;
+          continue;
+        }
 
-    // PointLights
-    for(i = 0; i < nbPointLights; i++)
-    {
-      cout << "IN POINTLIGHT :" << iss.str() << endl;
-      while(iss >> name_light >> position_x >> position_y >> position_z >> ambient_1 >> ambient_2 >> ambient_3 >> diffuse_1 >> diffuse_2 >> diffuse_3 >> specular_1 >> specular_2 >> specular_3 >> constant >> linear >> quadratic >> light_shader_name )
-      {
-        PointLight* pointLight = new PointLight(  
-          glm::vec3( (float)position_x , (float)position_y, (float)position_z),             
-          glm::vec3( (float)ambient_1, (float)ambient_2, (float)ambient_3),                 
-          glm::vec3( (float)diffuse_1 , (float)diffuse_2 , (float)diffuse_3),               
-          glm::vec3( (float)specular_1, (float)specular_2, (float)specular_3),              
-          (float)constant,
-          (float)linear,
-          (float)quadratic,
-          (string)light_shader_name
-          );
-        this->lights[name_light] = pointLight;
-      } // END WHILE
-      getline(file, line);
-      iss.str(line);
-    } // END FOR
+        //DIRLIGHTS
+        if(dirlights_initialization)
+        {
+          for(int i = 0; i < nbDirLights; i++)
+          {
+            istringstream iss(line);
+            while(iss >> name_light >> direction_x >> direction_y >> direction_z >> ambient_1 >> ambient_2 >> ambient_3 >> diffuse_1 >> diffuse_2 >> diffuse_3 >> specular_1 >> specular_2 >> specular_3 >> light_shader_name)
+            {
+              DirLight* dirLight = new DirLight(
+                glm::vec3( (float)direction_x,  (float)direction_y, (float)direction_z),            
+                glm::vec3( (float)ambient_1,    (float)ambient_2,   (float)ambient_3),                
+                glm::vec3( (float)diffuse_1,    (float)diffuse_2,   (float)diffuse_3),               
+                glm::vec3( (float)specular_1,   (float)specular_2,   (float)specular_3),
+                (string)light_shader_name
+                );              
 
-    //Detection of comments in config file
-    while (iss.str().find("#") != string::npos){
-      getline(file,line);
-      iss.str(line);
-    }
+              this->lights[name_light] = dirLight;
+              cout << name_light << endl;
+            }
 
-    // DirLights
-    for(i = 0; i < nbDirLights; i++)
-    {
-      cout << "IN DIRLIGHT :" << iss.str() << endl;
-      while(iss >> name_light >> direction_x >> direction_y >> direction_z >> ambient_1 >> ambient_2 >> ambient_3 >> diffuse_1 >> diffuse_2 >> diffuse_3 >> specular_1 >> specular_2 >> specular_3 >> light_shader_name)
-      {
-        DirLight* dirLight = new DirLight(
-          glm::vec3( (float)direction_x,  (float)direction_y, (float)direction_z),            
-          glm::vec3( (float)ambient_1,    (float)ambient_2,   (float)ambient_3),                
-          glm::vec3( (float)diffuse_1,    (float)diffuse_2,   (float)diffuse_3),               
-          glm::vec3( (float)specular_1,   (float)specular_2,   (float)specular_3),
-          (string)light_shader_name
-          );              
+            getline(file, line);
+          }
+          cout << "" << endl;
+          dirlights_initialization = false;
+          continue;
+        }
 
-        this->lights[name_light] = dirLight;
-      } // END WHILE
-      getline(file, line);
-      iss.str(line);
-    } // END FOR
+        //SPOTLIGHTS
+        if(spotlights_initialization)
+        {
+          for(int i = 0; i < nbSpotLights; i++)
+          {
+            istringstream iss(line);
+            while(iss >> name_light >> position_x >> position_y >> position_z >> direction_x >> direction_y >> direction_z >> cutOff >> outerCutOff >> constant >> linear >> quadratic >> ambient_1 >> ambient_2 >> ambient_3 >> diffuse_1 >> diffuse_2 >> diffuse_3 >> specular_1 >> specular_2 >> specular_3 >> light_shader_name)
+            {
+              SpotLight* spotLight = new SpotLight(  
+                glm::vec3( (float)position_x,   (float)position_y,  (float)position_z),
+                glm::vec3( (float)direction_x,  (float)direction_y, (float)direction_z),
+                (float) glm::cos(glm::radians(cutOff)),
+                (float) glm::cos(glm::radians(outerCutOff)),
+                (float)constant,
+                (float)linear,
+                (float)quadratic,
+                glm::vec3( (float)ambient_1,    (float)ambient_2,   (float)ambient_3),
+                glm::vec3( (float)diffuse_1,    (float)diffuse_2,   (float)diffuse_3),
+                glm::vec3( (float)specular_1,   (float)specular_2,  (float)specular_3),
+                (string)light_shader_name
+                );
 
-    //Detection of comments in config file
-    while (iss.str().find("#") != string::npos){
-      getline(file,line);
-      iss.str(line);
-    }
+              this->lights[name_light] = spotLight;
+              cout << name_light << endl;
+            }
 
-    // SpotLights
-    for(i = 0; i < nbSpotLights; i++)
-    {
-      cout << "IN SPOTLIGHTS " << iss.str() << endl;
-      while(iss >> name_light >> position_x >> position_y >> position_z >> direction_x >> direction_y >> direction_z >> cutOff >> outerCutOff >> constant >> linear >> quadratic >> ambient_1 >> ambient_2 >> ambient_3 >> diffuse_1 >> diffuse_2 >> diffuse_3 >> specular_1 >> specular_2 >> specular_3 >> light_shader_name)
-      {
-        SpotLight* spotLight = new SpotLight(  
-          glm::vec3( (float)position_x,   (float)position_y,  (float)position_z),
-          glm::vec3( (float)direction_x,  (float)direction_y, (float)direction_z),
-          (float) glm::cos(glm::radians(cutOff)),
-          (float) glm::cos(glm::radians(outerCutOff)),
-          (float)constant,
-          (float)linear,
-          (float)quadratic,
-          glm::vec3( (float)ambient_1,    (float)ambient_2,   (float)ambient_3),
-          glm::vec3( (float)diffuse_1,    (float)diffuse_2,   (float)diffuse_3),
-          glm::vec3( (float)specular_1,   (float)specular_2,  (float)specular_3),
-          (string)light_shader_name
-          );
+            getline(file, line);
+          }
+          cout << "" << endl;
+          spotlights_initialization = false;
+          continue;
+        }
 
+        //CHECKPOINTS
+        if(checkpoints_initialization)
+        {
+          for(int i = 0; i < nbCheckpoint; i++)
+          {
+            istringstream iss(line);
+            while(iss >> name_checkpoint >> rotate_angle >> rotate_x >> rotate_y >> rotate_z >> translate_x >> translate_y >> translate_z >> scale >> checkpoint_name_shader)
+            {            
+              this->checkpoints[ name_checkpoint ] = Checkpoint(checkpoint_name_shader, rotate_angle, rotate_x, rotate_y, rotate_z, translate_x, translate_y, translate_z, scale);
+              cout << name_checkpoint << endl;
+            }
 
-        this->lights[name_light] = spotLight;
-      } // END WHILE
-      getline(file, line);
-      iss.str(line);
-    } // END FOR
+            getline(file, line);
+          }
+          cout << "" << endl;
+          checkpoints_initialization = false;
+          continue;
+        }
 
-    cout << "" << endl;
+      } //End if comment
+    } //End read file
 
-
-    //CHECKPOINTS
-
-    //Detection of comments in config file
-    while (iss.str().find("#") != string::npos){
-      getline(file,line);
-      iss.str(line);
-    }
-
-    for(i = 0; i < nbCheckpoint; i++)
-    {
-      cout << "IN CHECKPOINT " << iss.str() << endl;
-      while(iss >> name_checkpoint >> rotate_angle >> rotate_x >> rotate_y >> rotate_z >> translate_x >> translate_y >> translate_z >> scale >> checkpoint_name_shader)
-      {
-        cout << name_checkpoint << endl;             
-        this->checkpoints[ name_checkpoint ] = Checkpoint(checkpoint_name_shader, rotate_angle, rotate_x, rotate_y, rotate_z, translate_x, translate_y, translate_z, scale);
-      }
-      if(i <nbCheckpoint-1){
-        getline(file, line); 
-        iss.str(line);
-      }
-    }
-    cout << "" << endl;
-    cout << "END : " << iss.str() << endl;
     file.close();
   }
-  else{
+
+  //Si le fichier n'existe pas
+  else 
+  {
     cerr << "Problème ouverture fichier de configuration" << endl;
   }
 
